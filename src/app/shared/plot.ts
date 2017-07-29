5a6bbeae953363b6304d38bfc49c0f4beec5f50d
Daniel Sun <hjdaniel.sun@gmail.com>
2017-07-29 13:15:42 +1000

    punched in some dummy plots

--- src/app/shared/plot.ts
+++ src/app/shared/plot.ts
@@ -10,8 +10,16 @@ export class Plot {
 
   public static createPlot(lifeTime: LifeTime): Plot {
     const plot = new Plot();
+    //TODO: do something smart about plotting your lifetime.
     plot._stories = [
-      Story.createStory("Born in ")
+      Story.createStory("Born in "),
+      Story.createStory("Growing up watching toy stories."),
+      Story.createStory("Went to University"),
+      Story.createStory("Hecs"),
+      Story.createStory("Jobs"),
+      Story.createStory("Got a deposit! $$$$"),
+      Story.createStory("Buying a house with 10% interest rate, paying it off in 10 years"),
+      Story.createStory("Oops, you are a female, Buying a house with 10% interest rate, pyaing it of in 20 years.")
     ];
 
     return plot;
